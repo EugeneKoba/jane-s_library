64ded205b96c3c40cc2d36a88ed283041b31064e
Eugene Annan <eugeneannan@Eugenes-MacBook-Pro.local>
2023-03-05 15:03:30 +0000

    Adding Async/Await to Fetch API for Book Search Section

--- script.js
+++ script.js
@@ -150,31 +150,32 @@ searchAuthorEl = document.querySelector(".search-author-el")
 
 
 // Function to search for the Book Title
-class SearchBooks {
-    static searchTitle() {
-        console.log("function Ran!")
-        // Fetch the OpenBook API. the 'q=' refers to the search result
-        const newResponse = fetch(`https://openlibrary.org/search.json?q=${searchInput.value}`)
-        .then((result) => {
-            return result.json()
-        })
-        .then((response) => {
-            if (!Response.ok) {
-                console.log("Oh No! An Error Occured!")
-                console.log(response)
-            } else {
-                console.log("It Worked!!")
-            }
-            for(let i=0;i<2;i++) {
-                searchTitleEl.innerHTML = `Title Name: ${response.docs[i].title}`
-            }
-        }
-        // .catch(function(err) {
-        //     console.log(`${err}, Oops, that didn't quite work)`)
-        // })
-    )}
+
+async function searchTitle() {
+    // Fetch the OpenBook API. the 'q=' refers to the search result
+    let response = await fetch(`https://openlibrary.org/search.json?q=${searchInput.value}`)
+    response = await response.json()
+    return response;
 }
 
-
 // Event Listener to listen for Search Button Click
-searchBtn.addEventListener("click", SearchBooks.searchTitle)
+searchBtn.addEventListener("click", async function() {
+    // Initially creating an empty array
+    let resultArray = []
+    // use 'try' to get the Promise response from searchTitle()
+    try {
+        resultArray = await searchTitle()
+    }
+    // catch an error
+    catch (error) {
+        console.log("Error!")
+        console.log(error)
+    }
+    // Print the response
+    for(let i=0;i<2;i++) {
+        searchTitleEl.innerHTML = `
+        Title: ${resultArray.docs[i].title}
+        Author: ${resultArray.docs[i].author_name}
+        Work: ${resultArray.docs[i].key}`
+    }
+})
