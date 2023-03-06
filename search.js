3b838b6a2ab38a9f870a524cd67c2ce69313f8c8
Eugene Annan <eugeneannan@Eugenes-MacBook-Pro.local>
2023-03-06 20:37:01 +0000

    Adding Title, Author, ISBN Search using Fetch API, with Openlibrary API + More Bootstrap 5 UI additions

--- search.js
+++ search.js
@@ -14,29 +14,32 @@
 // Taking in all inputs & buttons
 searchInput = document.querySelector(".search-input")
 searchBtn = document.querySelector(".search-btn")
-searchImgEl = document.querySelector(".search-img-el")
-searchTitleEl = document.querySelector(".search-title-el")
-searchAuthorEl = document.querySelector(".search-author-el")
-
+isbnEl = document.querySelector(".isbn-el")
+titleEl = document.querySelector(".title-el")
+authorEl = document.querySelector(".author-el")
+let bookWorks;
 
 
 
 // Function to search for the Book Title
 
-async function searchTitle() {
+async function getBooks() {
     // Fetch the OpenBook API. the 'q=' refers to the search result
     let response = await fetch(`https://openlibrary.org/search.json?q=${searchInput.value}`)
     response = await response.json()
+    console.log(response)
     return response;
 }
+
+
 
 // Event Listener to listen for Search Button Click
 searchBtn.addEventListener("click", async function() {
     // Initially creating an empty array
-    let resultArray = []
-    // use 'try' to get the Promise response from searchTitle()
+    let resultArray1 = []
+    // use 'try' to get the Promise response from getBooks()
     try {
-        resultArray = await searchTitle()
+        resultArray1 = await getBooks()
     }
     // catch an error
     catch (error) {
@@ -44,10 +47,24 @@ searchBtn.addEventListener("click", async function() {
         console.log(error)
     }
     // Print the response
-    for(let i=0;i<2;i++) {
-        searchTitleEl.innerHTML = `
-        Title: ${resultArray.docs[i].title}
-        Author: ${resultArray.docs[i].author_name}
-        Work: ${resultArray.docs[i].key}`
+    {
+        titleEl.innerHTML = `Title: ${resultArray1.docs[0].title}`
+        authorEl.innerHTML = `Author: ${resultArray1.docs[0].author_name}`
+        // Here, I'm getting the isbn of the book. to find it, I need to:
+        // 1a. find the Works of the book, which is located in the 'seed'.
+        let bookWorks = resultArray1.docs[0].seed[0]
+        console.log(bookWorks)
+        // 2. use the result, and execute a 2nd fetch, to get the isbn using the book works.
+        return fetch(`https://openlibrary.org${bookWorks}.json`)
+        .then(function(response2) {
+            return response2.json()
+        })
+        .then(function(response2) {
+            console.log(response2)
+            isbnEl.innerHTML = `
+            ISBN: ${response2.isbn_13}`
+        })
     }
 })
+
+
